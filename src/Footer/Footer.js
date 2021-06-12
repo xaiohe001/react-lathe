29491f534d3ce3a5c736bbe87d1e166f6c200ffd
LAPTOP-SHOB14OF\xiaozeng <2517460278@qq.com>
2021-06-12 22:14:08 +0800

    "6.12"

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,8 +2,51 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <div></div>
+        <div className="footer">
+            <div className="footer_top">
+                <div className="footer_top_center">
+                    <div>
+                        <i>友情链接 : </i>
+                        <strong>车床加工</strong>
+                        <span> | </span>
+                        <strong>数控车床加工</strong>
+                        <span> | </span>
+                        <strong>精密五金加工</strong>
+                        <span> | </span>
+                        <strong>铁丝网围栏</strong>
+                        <span> | </span>
+                        <strong>包头液压</strong>
+                        <span> | </span>
+                        <strong>网站宣传页</strong>
+                        <span> | </span>
+                        <strong>企业做网站</strong>
+                        <span> | </span>
+                        <strong>钻机</strong>
+                        <span> | </span>
+                        <strong>扣件</strong>
+                        <span> | </span>
+                        <strong>能量石手链</strong>
+                        <span> | </span>
+                        <strong>负离子项链</strong>
+                    </div>
+                </div>
+            </div>
+            <div className="footer_bottom">
+                <div className="footer_bottom_center">
+                    <div className="footer_bottom_top">
+                    Copyright © 2021, 小贺车床加工厂  All rights reserved.
+                    </div>
+                    <div className="footer_bottom_down">
+                    服务热线：10110110110  
+                    联系人：贺经理  
+                    邮箱：xiaohecat@qq.com  
+                    网址：www.110110.cn  
+                    地址：地球村
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-
+ 
 export default Footer
